082717a414335bd18833c1ec311aeb0ce7bc541c
Andrew Jones <aljones15@gmail.com>
2024-12-21 16:47:59 -0500

    Patch index.tsx & add blueprint to deps.

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import {createRoot} from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<div>START</div/)
+root.render(<div>START</div>)
