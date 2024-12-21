ff9d1033a04992991df798e2e82f44c0a42db812
Andrew Jones <aljones15@gmail.com>
2024-12-21 16:29:15 -0500

    Add basic data model + components & data dir.

---
+++ src/data/data-model.tsx
@@ -0,0 +1,18 @@
+// contains types for the resume
+
+interface Address = {
+  name: string;
+  address: [string];
+}
+
+const address: Address = {
+  name: 'Andrew Lyle Jones',
+  street: [
+    '2300 24th Rd South',
+    'Apartment 125'
+  ],
+  city: 'Alrington',
+  state: 'Virginia',
+  zipcode: '22206',
+  country: 'U.S.A'
+};
