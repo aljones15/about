0ea7b73150fd90718dc3a60248c498035eb28f94
Andrew Jones <aljones15@gmail.com>
2024-12-20 19:45:40 -0500

    Add minimal index,tsx.

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// aljones15/about homepage
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<div>START</div/)
