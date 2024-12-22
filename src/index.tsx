c5d0b470c87d6845dd6bb9903e0d2757436855a5
Andrew Jones <aljones15@gmail.com>
2024-12-22 18:00:20 -0500

    Prettier eslint corrections.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // aljones15/about homepage
-import React from 'react';
-import {createRoot} from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<div>START</div>)
+root.render(<div>START</div>);
