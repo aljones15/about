847ebc8ab9591b1ed8f9ed7bb7fa1f568798a587
Andrew Jones <aljones15@gmail.com>
2024-12-23 09:34:23 -0500

    Add initial types file.

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export interface Address {
+  street: Array<string>;
+  city: string;
+  state: string;
+  zipcode: string | number;
+  country: string;
+}
+
+export interface Company {
+  name: string;
+  homepage: URL;
+  github: URL | null;
+  linkedin: URL | null;
+  about: string | null
+}
+
+export interface Skill {
+  name: string;
+  about: string | null;
+  experience: number;
+}
+
+export interface Language {
+  name: string;
+  experience: number;
+}
+
+export interface Tool {
+  name: string;
+  experience: number;
+}
+
+export interface Position {
+  startDate: string;
+  endDate: string | null;
+  jobTitle: string;
+  company: Company;
+  languages: Array<Language>;
+  skills: Array<Skill>;
+  tootls: Array<Tool>;
+}
