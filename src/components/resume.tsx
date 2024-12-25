9503c5ad86cf597759dc3ad90e8cad14c6a86299
Andrew Jones <aljones15@gmail.com>
2024-12-25 08:04:13 -0500

    Add Navbar with icons and sections.

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import type {Resume} from "../data/resume";
 import Positions from "./position";
-import {Section, SectionCard} from "@blueprintjs/core";
+import {Button, Section, Navbar} from "@blueprintjs/core";
 
 export default function ResumeContainer({resume}: {resume: Resume}) {
 console.log({resume});
-  return <span className="row">
-    <Section className="col-xs-4">
-      <SectionCard padded={false}>
-        {resume.name}
-      </SectionCard>
-      <SectionCard padded={false}>
-        Address
-      </SectionCard>
-    </Section>
-    <Section className="col-xs-8">
-      <Positions positions={resume.positions} />
-    </Section>
-  </span>
+  return <Section>
+    <Navbar>
+      <Navbar.Group>
+      <Navbar.Heading>Andrew Lyle Jones</Navbar.Heading>
+      <Navbar.Divider />
+      <Button className="bp5-minimal" text="Experience" icon="applications" />
+      <Button className="bp5-minimal" text="Skills" icon="intelligence"/>
+      <Button className="bp5-minimal" text="Education" icon="manual"/>
+      <Button className="bp5-minimal" text="Bio" icon="user"/>
+      </Navbar.Group>
+    </Navbar>
+  </Section>
 }
