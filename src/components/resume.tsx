ee8fbc5b243f5cfdbcc6e2006a7a09db4103c02d
Andrew Jones <aljones15@gmail.com>
2025-01-13 15:54:12 -0500

    useLocation to set active.

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 import type {Resume} from "../data/resume";
 import Router from "./routes";
 import {Button, Section, SectionCard, Navbar} from "@blueprintjs/core";
@@ -13,6 +13,7 @@ const Sections = {
 
 export default function ResumeContainer({resume}: {resume: Resume}) {
   const navigate = useNavigate();
+  const location = useLocation();
   return <Section>
     <Navbar>
       <Navbar.Group>
@@ -22,21 +23,25 @@ export default function ResumeContainer({resume}: {resume: Resume}) {
         onClick={() => navigate(Sections.Experience)}
         className="bp5-minimal"
 	text="Experience"
+	active={location.pathname === Sections.Experience}
 	icon="applications" />
       <Button
-        onClick={() => navigate("/skills")}
+        onClick={() => navigate(Sections.Skills)}
         className="bp5-minimal"
 	text="Skills"
+	active={location.pathname === Sections.Skills}
 	icon="intelligence"/>
       <Button
         onClick={() => navigate(Sections.Education)}
         className="bp5-minimal"
 	text="Education"
+	active={location.pathname === Sections.Education}
 	icon="manual"/>
       <Button
         onClick={() => navigate(Sections.Bio)}
         className="bp5-minimal"
 	text="Bio"
+	active={location.pathname === Sections.Bio}
 	icon="user"/>
       </Navbar.Group>
     </Navbar>
