19070ad6aa42e5eb8b4caae0857b2f306fda36b2
Andrew Jones <aljones15@gmail.com>
2025-01-16 14:50:34 -0500

    Use collapsible Sections for Education.

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Education} from "../types";
+import {Classes, Section, SectionCard} from "@blueprintjs/core";
 
 export default function Education({education}: {education: Array<Education>}) {
   return <div>
-    Education
+    {education.map(ed => <School key={ed.name} school={ed} />)}
   </div>
+}
+
+export function School({school}: {school: Education}) {
+  return <Section
+    title={school.name}
+    icon="book"
+    collapsible={true}
+    className={`${Classes.ELEVATION_1}`}
+  >
+    {`${school.degree} ${school.field}`}
+  </Section>
 }
