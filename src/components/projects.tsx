212a3f3e792aba37120ace56f11a50827f7788ee
Andrew Jones <aljones15@gmail.com>
2025-01-23 12:22:49 -0500

    Start listing Projects in Cards.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import {Card, Tree, TreeNode} from "@blueprintjs/core";
 
 export default function Projects({projects}) {
-  return <div>Projects</div>
+  return <div className="projects">
+    {projects.map(project => <Card key={project.name}>
+      {project.name}
+    </Card>)}
+  </div>
 }
