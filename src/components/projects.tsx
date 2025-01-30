5ec5f1e163ef55a13ba715f946363b95da370b7d
Andrew Jones <aljones15@gmail.com>
2025-01-30 15:23:08 -0500

    Add initial reducer Switch.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,14 +4,20 @@ import {Card, Tree, TreeNodeInfo} from "@blueprintjs/core";
 type NodePath = number[];
 
 type ProjectAction =
-  {
+  | {
     type: "EXPAND_PROJECT";
     payload: {
       path: NodePath;
       isExpanded: boolean
     }
   }
-
+  | {
+    type: "COLLAPSE_PROJECT";
+    payload: {
+      path: NodePath;
+      isExpanded: boolean
+    }
+  };
 
 export default function Projects({projects}) {
   const initialNodes = projects.map(project => ({
@@ -21,17 +27,49 @@ export default function Projects({projects}) {
     label: project.name,
     icon: "archive",
     childNodes: [{
-
+      id: "project-description",
+      key: project.description.join(""),
+      isExpanded: false,
+      label: "Description",
+      icon: "comment"
     }]
   }));
   const [nodes, dispatch] = React.useReducer(projectReducer, initialNodes);
   return <Card className="projects">
     <Tree
+      onNodeExpand={(n, path: NodePath) => dispatch({
+        type: "EXPAND_PROJECT",
+	payload: {
+	  isExpanded: true,
+	  path
+	}
+      })}
+      onNodeCollapse={(n, path: NodePath) => dispatch({
+        type: "COLLAPSE_PROJECT",
+	payload: {
+          isExpanded: false,
+	  path
+        }
+      })}
       contents={nodes} />
   </Card>
 }
 
 function projectReducer(state: TreeNodeInfo[], action: ProjectAction) {
-console.log({action});
-  return state;
+  // copy the state
+  const newState = structuredClone(state);
+  switch(action.type) {
+    case "COLLAPSE_PROJECT": {
+      return newState;
+    }
+    case "EXPAND_PROJECT": {
+      return newState;
+    }
+    default:
+      return newState;
+  }
+}
+
+function visitor(state: TreeNodeInfo[], path: NodePath) {
+
 }
