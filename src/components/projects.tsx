aa03285be4d1d4ae469ecc81fbc0e9293af59dfc
Andrew Jones <aljones15@gmail.com>
2025-01-30 14:54:20 -0500

    useReducer for Tree Nodes.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import {Card, Tree, TreeNodeInfo} from "@blueprintjs/core";
 
 export default function Projects({projects}) {
-  const nodes = [];
+  const initialNodes = projects.map(project => ({
+    id: project.name,
+    key: project.name,
+    isExpanded: true,
+    label: project.name,
+    icon: "archive",
+    childNodes: [{
+
+    }]
+  }));
+  const [nodes, dispatch] = React.useReducer(projectReducer, initialNodes);
   return <Card className="projects">
-    <Tree contents={nodes} />
+    <Tree
+      contents={nodes} />
   </Card>
+}
+
+function projectReducer(state: TreeNodeInfo[]) {
+  return state;
 }
