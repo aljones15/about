3902b49d8d467d5a90dc6fdbb8fb145d2d3cc4ac
Andrew Jones <aljones15@gmail.com>
2025-02-02 11:36:33 -0500

    Break projects into seperate Trees.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,18 +4,17 @@ import { Framework } from "./frameworks";
 import { Language } from "./languages";
 import { Skill } from "./skills";
 import { Tool } from "./tools";
-import { iAbilities } from "../types";
+import { iProject } from "../types";
 
 type NodePath = number[];
 
 type ProjectAction =
-  | {
+  {
     type: "EXPAND_PROJECT";
     payload: {
       path: NodePath;
     }
-  }
-  | {
+  } | {
     type: "COLLAPSE_PROJECT";
     payload: {
       path: NodePath;
@@ -23,83 +22,15 @@ type ProjectAction =
   };
 
 export default function Projects({projects}) {
-  const initialNodes = structuredClone(projects).map(project => ({
-    id: project.name,
-    key: project.name,
-    isExpanded: true,
-    label: project.name,
-    icon: "application",
-    childNodes: [{
-      id: "project-description",
-      key: project.description.join(""),
-      isExpanded: true,
-      label: "Description",
-      icon: "comment",
-      childNodes: project.description.map(d => ({
-        id: d,
-	key: d,
-	label: d,
-	icon: "symbol-circle"
-      }))
-    }, {
-      id: "project-skills",
-      key: project.name + project.skills.join(""),
-      isExpanded: false,
-      label: "Skills",
-      icon: "exchange",
-      childNodes: [{
-        id: project.name + "skills",
-	key: project.name + "skills",
-	label: <div>{project.skills.map(skill => <Skill
-	  key={project.name + skill.name}
-	  skill={skill} />
-	)}</div>
-      }]
-    }, {
-      id: "project-frameworks",
-      key: project.name + project.frameworks.join(""),
-      isExpanded: false,
-      label: "Frameworks",
-      icon: "package",
-      childNodes: [{
-        id: project.name + "fws",
-	key: project.name + "fws",
-	label: <div>{project.frameworks.map(fw => <Framework
-	  key={project.name + fw.name}
-	  framework={fw} />
-	)}</div>
-      }]
-    }, {
-      id: project.name + "languages",
-      key: project.name + project.languages.join(""),
-      isExpanded: false,
-      label: "Languages",
-      icon: "translate",
-      childNodes: [{
-        id: project.name + "languages",
-	key: project.name + project.languages.join(""),
-	label: <div>{project.languages.map(l => <Language
-	  key={project.name + l.name}
-	  language={l} />
-	)}</div>
-      }]
-    }, {
-      id: project.name + "tools",
-      key: project.name + project.tools.join(""),
-      isExpanded: false,
-      label: "Tools",
-      icon: "wrench",
-      childNodes: [{
-        id: project.name + "tools",
-	key: project.name + project.tools.join(""),
-	label: <div>
-	  {project.tools.map(t => <Tool key={project.name + t.name} tool={t} />)}
-	</div>
-      }]
-    }
-  ]
-  }));
-  const [nodes, dispatch] = React.useReducer(projectReducer, initialNodes);
+  return <div>
+    {projects.map(project => <ProjectCard
+      key={project.name}
+      project={project} />)}
+  </div>
+}
+
+function ProjectCard({project}) {
+  const [nodes, dispatch] = React.useReducer(projectReducer, [createInitialNode(project)]);
   return <Card className="projects">
     <Tree
       onNodeExpand={(n, path: NodePath) => dispatch({
@@ -112,6 +43,74 @@ export default function Projects({projects}) {
       })}
       contents={nodes} />
   </Card>
+}
+
+function createInitialNode(project: iProject): TreeNodeInfo {
+  return {
+    id: project.name,
+    isExpanded: true,
+    label: project.name,
+    icon: "application",
+    childNodes: [{
+      id: "project-description",
+      isExpanded: true,
+      label: "Description",
+      icon: "comment",
+      childNodes: project.description.map(d => ({
+        id: d,
+	key: d,
+	label: d,
+	icon: "symbol-circle"
+      }))
+    }, {
+      id: "project-skills",
+      isExpanded: false,
+      label: "Skills",
+      icon: "exchange",
+      childNodes: [{
+        id: project.name + "skills",
+	label: <div>{project.skills.map(skill => <Skill
+	  key={project.name + skill.name}
+	  skill={skill} />
+	)}</div>
+      }]
+    }, {
+      id: "project-frameworks",
+      isExpanded: false,
+      label: "Frameworks",
+      icon: "package",
+      childNodes: [{
+        id: project.name + "fws",
+	label: <div>{project.frameworks.map(fw => <Framework
+	  key={project.name + fw.name}
+	  framework={fw} />
+	)}</div>
+      }]
+    }, {
+      id: project.name + "languages",
+      isExpanded: false,
+      label: "Languages",
+      icon: "translate",
+      childNodes: [{
+        id: project.name + "languages",
+	label: <div>{project.languages.map(l => <Language
+	  key={project.name + l.name}
+	  language={l} />
+	)}</div>
+      }]
+    }, {
+      id: project.name + "tools",
+      isExpanded: false,
+      label: "Tools",
+      icon: "wrench",
+      childNodes: [{
+        id: project.name + "tools",
+	label: <div>
+	  {project.tools.map(t => <Tool key={project.name + t.name} tool={t} />)}
+	</div>
+      }]
+    }]
+  }
 }
 
 function projectReducer(state: TreeNodeInfo[], action: ProjectAction) {
