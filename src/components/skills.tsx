1aa72909d488b5d55bb227202f4e315485bccc03
Andrew Jones <aljones15@gmail.com>
2025-02-19 16:56:50 -0500

    Remove PanelStack2 Attempt from Abilities.

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Button,
   Callout, CardList,
   Card, Colors,
   PanelStack,
@@ -11,62 +10,17 @@ import {
  } from "@blueprintjs/core";
 import {iAbility, iPosition, iSkill, iFramework, iLanguage, iTool} from "../types";
 
-const testPanel: Panel<object> = {
-  props: {foo: true},
-  renderPanel: (props) => {
-    return <Button text="Text" />
-  },
-  title: "I.P.",
-  htmlTitle: "Test Panel"
-}
-
-const testComp = () => <Button text="TEST" />
-
 export default function Skills({positions}: {positions: Array<iPosition>}) {
   const languages = new Set(positions.flatMap(p => p.languages));
   const skills = new Set(positions.flatMap(p => p.skills));
   const tools = new Set(positions.flatMap(p => p.tools));
   const frameworks = new Set(positions.flatMap(p => p.frameworks));
-  const [stack, updateStack] = React.useState([testPanel]);
   return <Card compact={true} className="row">
-  {/*
     <SkillSection entries={frameworks} title="Frameworks" color="GREEN"/>
     <SkillSection entries={skills} title="Skills" color="GOLD"/>
     <SkillSection entries={languages} title="Languages" color="BLUE"/>
     <SkillSection entries={tools} title="Tools" color="RED"/>
-  */}
-  <PanelStack2 stack={stack} showPanelHeader={false} renderActivePanelOnly={true} />
   </Card>
-}
-
-interface iPanel<T extends object>{
-  entries: Set<T>;
-  title: string;
-  color: string;
-};
-
-const AbilityPanel: React.FC<PanelProps<object>> = props => {
-  return (<div>Panel Test</div>);
-}
-
-
-
-function AbilityPanels<T extends object>({
-  entries,
-  title,
-  color
-}: iPanel<T>) {
-  const [initialPanel, ...stack]: Panel<object>[] = [...entries].map(
-    (entry: T, index: number): Panel<object> => ({
-    props: {entry},
-    renderPanel: AbilityPanel,
-    title: `Panel Test ${index}`
-  }));
-  return <PanelStack2
-    showPanelHeader={true}
-    initialPanel={testPanel}
-    stack={stack}
-  />
 }
 
 function SkillSection({
