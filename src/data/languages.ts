ca1664b8cb4fa02737b530956fdc3b723d73b7fa
Andrew Jones <aljones15@gmail.com>
2025-05-10 20:47:32 -0400

    Add basics to Languages.

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -2,49 +2,65 @@ import { Language } from "../classes/Language";
 
 export const javascript: Language = new Language({
   name: 'Javascript',
+  comments: [],
   experience: 10,
   functional: true
 });
 
 export const typescript: Language = new Language({
   name: 'Typescript',
+  comments: [],
   experience: 1,
   functional: true
 });
 
 export const csharp: Language = new Language({
   name: 'C#',
-  experience: 3
+  comments: [],
+  experience: 3,
+  functional: false
 });
 
 export const python: Language = new Language({
   name: 'Python',
-  experience: 1
+  comments: [],
+  experience: 1,
+  functional: true
 });
 
 export const java: Language = new Language({
-  name: 'Java',
-  experience: 1
+    name: 'Java',
+    comments: [],
+    experience: 1,
+    functional: false
 });
 
 export const php: Language = new Language({
   name: 'PHP',
-  experience: 3
+  comments: [],
+  experience: 3,
+  functional: false
 });
 
 export const R: Language = new Language({
   name: "R",
-  experience: 5
+  comments: [],
+  experience: 5,
+  functional: true
 });
 
 export const css: Language = new Language({
   name: "CSS",
-  experience: 10
+  comments: [],
+  experience: 10,
+  functional: false
 });
 
 export const jsonld: Language = new Language({
   name: "JSON-LD",
-  experience: 5
+  comments: [],
+  experience: 5,
+  functional: false
 });
 
 export const languages: Language[] = [
