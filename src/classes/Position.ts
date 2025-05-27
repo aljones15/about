0fc2fecc083491fe2c20f07e49c3dc5326682865
Andrew Jones <aljones15@gmail.com>
2025-05-26 20:04:42 -0400

    Add Position Class.

--- src/classes/Position.ts
+++ src/classes/Position.ts
@@ -1,7 +1,31 @@
 import { iPosition } from "../types";
+import { Language } from "./Language";
+import { Framework } from "./Framework";
+import { Skill } from "./Skill";
+import { Tool } from "./Tool";
 
 export class Position {
-  constructor({}: iPosition) {
-
+  jobTitle: string;
+  company: iCompany;
+  languages: Array<Language>;
+  frameworks: Array<Framework>;
+  skills: Array<Skill>;
+  tools: Array<Tool>;
+  duties: Array<string>;
+  startDate: string;
+  endDate: string | null;
+  constructor({
+    jobTitle, company, languages,
+    frameworks, skills, tools,
+    duties, startDate, endDate = null
+  }: iPosition) {
+    this.jobTitle = jobTitle;
+    this.company = company;
+    this.languages = languages;
+    this.skills = skills;
+    this.tools = tools;
+    this.duties = duties;
+    this.startDate = startDate;
+    this.endDate = endDate;
   }
 }
